4f4e8c74b3e677c7036ccdafa930aa2fead2b41a
Paul Ogolla <paulotieno2@gmail.com>
2024-10-26 15:35:21 +0300

    setup models

---
+++ models/exercise_log.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const exerciseLogSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  exercise: {
+    type: String,
+    required: true,
+  },
+  sets: {
+    type: Number,
+    required: true,
+  },
+  reps: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+const ExerciseLog = mongoose.model('ExerciseLog', exerciseLogSchema);
+
+module.exports = ExerciseLog;
